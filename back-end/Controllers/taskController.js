58a6f4bde7dcce9c7841b39ee23d066db515187f
Salma-Moh <salmamoh168@gmail.com>
2024-10-22 21:00:27 +0300

    Updated login and signup forms to clear input and navigate to login

--- back-end/Controllers/taskController.js
+++
@@ -1,8 +0,0 @@
-const Task = require("../Models/taskModel");
-const factory = require("./handlerFactory");
-
-exports.createTask = factory.createOne(Task);
-exports.getAllTasks = factory.getAll(Task);
-exports.getTask = factory.getOne(Task);
-exports.updateTask = factory.updateOne(Task);
-exports.deleteTask = factory.deleteOne(Task);
--- back-end/README.md
+++
@@ -1,2 +0,0 @@
-# BackEnd
- Depi BackEnd Part
